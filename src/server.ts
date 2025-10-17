7623f38ec18a6db0e45ba267057ddad258e8bde5
Turarbek-2005 <turarbek.kabdesh@mail.ru>
2025-10-18 02:04:34 +0500

    fix

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ app.use(compression());
 app.use(express.json());
 
 app.use("/api/f1",authMiddleware, f1Router);
-app.use("/api/auth",authMiddleware, authRouter);
+app.use("/api/auth", authRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
