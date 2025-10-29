bec2b842ba88c89ea92f4da7996edb7a84690193
Turarbek-2005 <turarbek.kabdesh@mail.ru>
2025-10-29 23:16:01 +0500

    backend fix

--- backend/src/routes/f1api.routes.ts
+++ backend/src/routes/f1api.routes.ts
@@ -10,31 +10,41 @@ const axiosClient = axios.create({
   timeout: 5000,
 });
 
-async function fetchFromF1Api(
-  path: string,
-  res: Response,
-  cacheName?: string
-) {
-  try {
-    const { data } = await axiosClient.get(path);
+async function fetchFromF1Api(path: string, res: Response, cacheName?: string) {
+  logger.info(`[F1 API] Fetching data from path: ${path}`);
 
+  let cleanPath = (path ?? "").replace(/^\/+/, "");
+
+  if (/search/i.test(cleanPath)) {
+    cleanPath = cleanPath.replace(/^current\//i, "");
+  }
+
+  const base = axiosClient.defaults.baseURL ?? API_BASE;
+  const fullUrlForLog = `${base}${cleanPath}`;
+  logger.info(`[F1 API] request -> ${fullUrlForLog}`);
+
+  try {
+    const { data } = await axiosClient.get(cleanPath);
     return res.json(data);
   } catch (error: any) {
-    logger.error(` [F1 API] ${path} — ${error.message}`);
+    logger.error(` [F1 API] ${cleanPath} — ${error.message}`);
     return res
       .status(502)
       .json({ message: "Error fetching data from external F1 API" });
   }
 }
 
+
+
 //////////////////////////* Drivers *////////////////////////////
 
 router.get("/drivers", (req: Request, res: Response) =>
   fetchFromF1Api("current/drivers", res, "current/drivers")
 );
-router.get("/drivers/:driverId", (req: Request, res: Response) =>
-  fetchFromF1Api("current/drivers/:driverId", res, "current/drivers/:driverId")
-);
+router.get("/drivers/:driverId", (req: Request, res: Response) =>{
+  const { driverId } = req.params;
+  return fetchFromF1Api(`current/drivers/${driverId}`, res, `current/drivers/${driverId}`);
+});
 
 router.get("/drivers/search", async (req: Request, res: Response) => {
   const query = req.query.q as string;
@@ -61,7 +71,7 @@ router.get("/teams/:teamId/drivers", (req: Request, res: Response) => {
   return fetchFromF1Api(`current/teams/${teamId}/drivers`, res, `current/teams/${teamId}/drivers`);
 });
 
-router.get("/search", async (req: Request, res: Response) => {
+router.get("/teams/search", async (req: Request, res: Response) => {
   const query = req.query.q as string;
 
   if (!query)
@@ -74,25 +84,25 @@ router.get("/search", async (req: Request, res: Response) => {
 //////////////////////////* Results *////////////////////////////
 
 router.get("/last/fp1", (req: Request, res: Response) =>
-  fetchFromF1Api("/current/last/fp1", res, "/current/last/fp1")
+  fetchFromF1Api("current/last/fp1", res, "/current/last/fp1")
 );
 router.get("/last/fp2", (req: Request, res: Response) =>
-  fetchFromF1Api("/current/last/fp2", res, "/current/last/fp2")
+  fetchFromF1Api("current/last/fp2", res, "/current/last/fp2")
 );
 router.get("/last/fp3", (req: Request, res: Response) =>
-  fetchFromF1Api("/current/last/fp3", res, "/current/last/fp3")
+  fetchFromF1Api("current/last/fp3", res, "/current/last/fp3")
 );
 router.get("/last/qualy", (req: Request, res: Response) =>
-  fetchFromF1Api("/current/last/qualy", res, "/current/last/qualy")
+  fetchFromF1Api("current/last/qualy", res, "/current/last/qualy")
 );
 router.get("/last/race", (req: Request, res: Response) =>
-  fetchFromF1Api("/current/last/race", res, "/current/last/race")
+  fetchFromF1Api("current/last/race", res, "/current/last/race")
 );
 router.get("/last/sprint/qualy", (req: Request, res: Response) =>
-  fetchFromF1Api("/current/last/sprint/qualy", res, "/current/last/sprint/qualy")
+  fetchFromF1Api("current/last/sprint/qualy", res, "/current/last/sprint/qualy")
 );
 router.get("/last/sprint/race", (req: Request, res: Response) =>
-  fetchFromF1Api("/current/last/sprint/race", res, "/current/last/sprint/race")
+  fetchFromF1Api("current/last/sprint/race", res, "/current/last/sprint/race")
 );
 
 //////////////////////////* Standings *////////////////////////////
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -11,11 +11,18 @@ interface Team {
 
 export default function Home() {
   const [teams, setTeams] = useState<Team[] | null>(null);
-
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMsInVzZXJuYW1lIjoiZG9uayIsImlhdCI6MTc2MTQxMDY1OCwiZXhwIjoxNzYyMDE1NDU4fQ.PNwnPnuohCvETnYq-GBIozlitBUnu-A74JUP6NViwos";
   useEffect(() => {
     async function fetchTeams() {
       try {
-        const res = await fetch("http://localhost:4200/api/f1/teams");
+        const res = await fetch("http://localhost:4200/api/f1/teams", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
         const data = await res.json();
         console.log("Загруженный пилот:", data);
         setTeams(data);
@@ -27,20 +34,28 @@ export default function Home() {
     fetchTeams();
   }, []);
 
-  if (!teams) return <div className="container mx-auto text-center">Loading...</div>;
+  if (!teams)
+    return <div className="container mx-auto text-center">Loading...</div>;
 
   return (
     <div className="container mx-auto text-center mt-10">
       <h1 className="text-3xl font-bold mb-4">Welcome to F1KZ!</h1>
       <div className="grid grid-cols-2 gap-4 mb-8">
-
-      {teams.map((team) => (
-        <div key={team.id} >
-          <h2 className="text-xl font-semibold">{team.teamId}</h2>
-          <img src={team.teamImgUrl} alt={team.teamImgUrl} className="mx-auto w-10 h-10 " />
-          <img src={team.bolidImgUrl} alt={team.bolidImgUrl} className="mx-auto w-96 h-auto " />
-        </div>
-      ))}
+        {teams.map((team) => (
+          <div key={team.id}>
+            <h2 className="text-xl font-semibold">{team.teamId}</h2>
+            <img
+              src={team.teamImgUrl}
+              alt={team.teamImgUrl}
+              className="mx-auto w-10 h-10 "
+            />
+            <img
+              src={team.bolidImgUrl}
+              alt={team.bolidImgUrl}
+              className="mx-auto w-96 h-auto "
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
